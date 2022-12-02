70d3f323a0869ba920798259e93c3fafb949b071
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-12-02 16:38:13 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SampleUnitTest;
+
+namespace BankAccountsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+           
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccounts account = new BankAccounts("Ragini Singh", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
